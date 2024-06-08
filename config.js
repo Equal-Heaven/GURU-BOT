d22a2856821aaa5deb093753ffa928700c628e17
Equal-Heaven <145233988+Equal-Heaven@users.noreply.github.com>
2024-06-08 23:59:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "242068312525;br.x;242068312525;𝕭𝖗.𝖝"
 
 
 const ownerlist = ownervb.split(';');
@@ -21,9 +21,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
     global.owner.push(owner);
 }
 
-//global.pairingNumber = "" //put your bot number here
+global.pairingNumber = "242068312525" //put your bot number here
 
-global.mods = ['917605902011', '32460248586']
+global.mods = ['242067512708', '242064064148']
 global.prems = ['917605902011', '32460248586', '919398758484']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -71,12 +71,12 @@ global.APIKeys = {
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
+global.packname = '𝕎𝔼𝕀ℝ𝔻-𝕋𝔼𝔸𝕄'
+global.author = '𝕭𝖗.𝖝'
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/guru.json\n'
-global.dygp = 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT'
+global.dygp = 'https://chat.whatsapp.com/Cs3KweAEXyqJxnLVzAKeYR'
+global.fgsc = 'https://github.com/Equal-Heaven/GURU-BOT'
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
